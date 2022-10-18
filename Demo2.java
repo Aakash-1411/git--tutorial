492c3ebca06a4a2df766e10b10758897e7fd8c5a
Aakash Waghulde <aakashwaghulde@gmail.com>
2022-10-18 11:25:02 +0530

    final commit

--- Demo2.java
+++ Demo2.java
@@ -1,5 +1,6 @@
 public class Demo2 {
     public static void main(String[] args) {
         System.out.println("Helloooo world...!!!");
+        System.out.println("This is Git learning");
     }
 }
